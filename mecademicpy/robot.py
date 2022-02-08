cd905e92580bfa94a4470aba967eaa740e53a1bd
Alain Brassard <alain.brassard@mecademic.com>
2022-02-08 14:36:41 -0500

    ROBOT-1844: Code review suggestion: - Do not expose "is_test_bench" in the Python public API. Only do this in a "private" version of the Robot class.

--- mecademicpy/robot.py
+++ mecademicpy/robot.py
@@ -735,13 +735,11 @@ class RobotInfo:
     def __init__(self,
                  model: str = None,
                  revision: int = None,
-                 is_test_bench: bool = None,
                  is_virtual: bool = None,
                  version: RobotVersion = None,
                  serial: str = None):
         self.model = model
         self.revision = revision
-        self.is_test_bench = is_test_bench
         self.is_virtual = is_virtual
         self.version = RobotVersion(version)
         self.serial = serial
@@ -783,9 +781,8 @@ class RobotInfo:
 
         """
         ROBOT_CONNECTION_STRING = \
-            r"Connected to (?P<model>\w+) ?R?(?P<revision>\d)?(?P<test_bench>-TB)?(?P<virtual>-virtual)?( v|_)?(?P<version>\d+\.\d+\.\d+)"
+            r"Connected to (?P<model>\w+) ?R?(?P<revision>\d)?(?P<virtual>-virtual)?( v|_)?(?P<version>\d+\.\d+\.\d+)"
 
-        test_bench = False
         virtual = False
         try:
             robot_info_regex = re.search(ROBOT_CONNECTION_STRING, input_string)
@@ -793,15 +790,9 @@ class RobotInfo:
                 model = robot_info_regex.group('model')
             if robot_info_regex.group('revision'):
                 revision = int(robot_info_regex.group('revision'))
-            if robot_info_regex.group('test_bench'):
-                test_bench = True
             if robot_info_regex.group('virtual'):
                 virtual = True
-            return cls(model=model,
-                       revision=revision,
-                       is_test_bench=test_bench,
-                       is_virtual=virtual,
-                       version=robot_info_regex.group('version'))
+            return cls(model=model, revision=revision, is_virtual=virtual, version=robot_info_regex.group('version'))
         except Exception as exception:
             raise ValueError(f'Could not parse robot info string "{input_string}", error: {exception}')
 
@@ -3250,7 +3241,7 @@ class Robot:
         Return
         ------
         string
-            Name of the zip file that contains robot information an captured trajectory
+            Name of the zip file that contains robot information and captured trajectory
         """
         if self._file_logger is None:
             raise InvalidStateError('No existing logger to stop.')
@@ -3615,7 +3606,7 @@ class Robot:
                 self._send_command(f'SyncCmdQueue({self._tx_sync})')
             if event.is_set():
                 event.clear()
-                self._send_command(command)
+            self._send_command(command)
 
         # Wait until response is received (this will throw TimeoutException if appropriate)
         event.wait(timeout=timeout)
